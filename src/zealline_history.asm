43df4fe7b3969a0fe54ef294d5bda1e27b6fa279
Zeal8bit <contact@zeal8bit.com>
2025-06-20 21:15:04 +0800

    refactor: get rid of the IX/IY registers in the history manager, saving +300 bytes

--- src/zealline_history.asm
+++ src/zealline_history.asm
@@ -18,11 +18,6 @@
         ;PUBLIC zealline_history_search_backward_substr
         PUBLIC zealline_add_history
 
-        EXTERN OutputRegisters
-        EXTERN OutputMemoryAtDE
-        EXTERN OutputNewline
-
-
         ; History
         ASSERT(HISTORY_SIZE >= 512)     ; the ring buffer must be at least big enough to store 2 commands
                                         ; just to be super save that deleting (some) entries will always create
@@ -52,18 +47,17 @@
                 add 1                           ; so we end on the address of the next item in a aligned way
         ENDM
 
-        ; Checks if the adress in HL is still before the end of the history ringbuffer
-        ;   Jumps to the labels
+        ; Checks if the adress in HL is beyond the history ringbuffer. Jumps if valid.
         ; Alters: A
-        MACRO ON_HL_BEYOND_HISTORY_RINBUFFER beyond_label, not_beyond_label
-                ld a, h                                 ; Compare high byte of HL with high byte of history_ringbuffer_end
+        MACRO ON_REG_VALID_GOTO upper, lower, valid_label
+                ld a, upper                             ; Compare high byte of HL with high byte of history_ringbuffer_end
                 cp history_ringbuffer_end >> 8
-                jr C, not_beyond_label
+                jr c, valid_label
 
-                ld a, l                                 ; Compare low byte of HL with low byte of history_ringbuffer_end
+                ld a, lower                             ; Compare low byte of HL with low byte of history_ringbuffer_end
                 cp history_ringbuffer_end & 0xFF
-                jr C, not_beyond_label
-                jr beyond_label
+                jr c, valid_label
+                ; Fall-through
         ENDM
 
         ; Tests if HL is a null byte
@@ -75,31 +69,30 @@
         ENDM
 
 
-        ; Tests if DE is not equal to BC
-        ; Alters: A
-        MACRO DE_NE_BC label
-        ld a, d
-        cp b
-        jr nz, label ; If high bytes are different, jump
-
-        ld a, e
-        cp c
-        jr nz, label ; If low bytes are different, jump
-
-        ; If we reach here, DE == BC (so we don't jump)
-        ENDM
-
-
         ; Setup HL and BC for all zealline_history_search*
         ; Returns: HL and BC, the result values
         MACRO SETUP_SEARCH_RESULT _
-                ld hl, history_search_result
-                ld ix, (history_iterator_ptr)
+                ld hl, (history_iterator_ptr)
+                ; Point to the length
+                inc l
+                inc l
                 ld b, 0
-                ld c, (ix+history_entry_line_len)
+                ld c, (hl)
                 dec c                                   ; dec c because remove nullbyte from length
+                ld hl, history_search_result
         ENDM
 
+
+        ; Align HL on a 4-byte bound
+align_hl:
+        ; HL = (HL + 3) & ~3
+        inc hl
+        inc hl
+        inc hl
+        ld a, 0xFC
+        and l
+        ld l, a
+        ret
 
         ; Resets the history serach iterator
         ; Alters: HL
@@ -111,7 +104,7 @@ zealline_reset_history_search:
         ; Searches backward through the history, retrieving the previous line.
         ; Returns: HL - the pointer to the line
         ;          BC - length of the line
-        ; Alters: IX, A, HL
+        ; Alters: A, HL
 zealline_history_search_backward:
         push de
         ld bc, 0
@@ -170,133 +163,154 @@ zealline_add_history:
         ld a, c                                         ; C is stringlength
         cp MAX_LINE_LENGTH
         jp nc, _add_history_error
+        inc c                                           ; line length: Add 1 for NULL Byte
         ex de, hl                                       ; store line in DE
         ld hl, (history_current_ptr)
-        inc c                                           ; line length: Add 1 for NULL Byte
         ON_HL_IS_NULL_GOTO(_add_history_first_entry)    ; Add the first Element
         ; regular insert into the ringbuffer
-        ld a, c
-        ADD_HISTORY_ALIGNMENT()
-        ld b, a                                         ; entry length: store in B
+        ; Keep the buffer and length on the stack
+        push de
+        push bc
         ; Calculate the address were we are going to write to
-        ld ix, (history_current_ptr)                    ; Load the address of the entry into HL and IX
-        ld hl, ix
-        ld a, (ix+history_entry_line_len)               ; Load the length of that entry into A
-        ADD_HISTORY_ALIGNMENT()
+        ld hl, (history_current_ptr)                    ; Load the address of the entry into HL and IX
+        ASSERT(history_entry_line_len == 2)
+        ; HL is aligned on 4 for sure
+        inc l
+        inc l
+        ld a, (hl)                                      ; Load the length of that entry into A
+        inc l                                           ; Point on the string
+        ; ADD_HISTORY_ALIGNMENT()
         ADD_HL_A()                                      ; => HL points to the next address we want to write to
-        ON_HL_BEYOND_HISTORY_RINBUFFER(_add_history_wrap_hl_into_ringbuffer, _add_history_check_for_space)
-_add_history_wrap_hl_into_ringbuffer:
-        ld ix, bc                                       ; STORE BC to IX
-        ld bc, HISTORY_SIZE
-        or a                                            ; Remove Carry Flag
-        sbc hl, bc                                      ; subtract rinbuffer_size from HL
-        ld bc, ix                                       ; RESTORE BC
+        call align_hl
+        ON_REG_VALID_GOTO(H, L, _add_history_check_for_space)
+        ld a, b
+        ld bc, -HISTORY_SIZE
+        add hl, bc                                      ; subtract rinbuffer_size from HL
 _add_history_check_for_space:
+        ld b, a                                         ; Put the size back is B
         call is_history_space_available                 ; checks if we have enough space in the ringbuffer
         or a
         jp z, _add_history_add_entry
         ; Drop the element - Because of the alignment this is happening without potential wrap-around
-        push de                                         ; save DE
-        ld ix, (history_current_ptr)
-        ld iy, (ix+history_entry_next)                  ; iy - address of entry that should be removed
-        ld de, (iy+history_entry_next)                  ; de - address of the entry that becomes the new next entry
-        ld (ix+history_entry_next), de                  ; store it to current entry
+        ; Save HL in BC
+        ld b, h
+        ld c, l
+        ; Performs `current.next = current.next.next`
+        ld hl, (history_current_ptr)
+        push hl
+        ld a, (hl)
+        inc l
+        ld h, (hl)
+        ld l, a
+        ; HL points to the "next"'s next entry, copy it to the current's next
         pop de
+        ldi
+        ldi
+        ; Restore HL
+        ld h, b
+        ld l, c
         jp _add_history_check_for_space
 _add_history_add_entry:
         ; Append history element to HL
-        ld ix, hl                                       ; IX - address of new entry
-        ld hl, (history_current_ptr)                    ; load current pointer - previous entry
-        ld (history_current_ptr), ix                    ; write the new pointer.
-        ; the entry at current_ptr should point to the element we are creating
-        ; but it points to the next element, we need to point to that element!
+        ; Get the former current entry in HL and the new entry in DE
+        ex de, hl
+        ld hl, (history_current_ptr)
+        ; Set the new entry as the current
+        ld (history_current_ptr), de
+        ; Copy the "next" field from the former current to the new entry
+        ; new_entry.next[0] = former_entry.next[0]
         ASSERT history_entry_next==0
-        ; inc hl, history_entry_next - is a nop because history_entry_next is 0
-        ld iy, (hl)                                     ; address of next entry
-        ; write the header of new entry
-        ld a, iyl
-        ld (ix+history_entry_next), a                   ; addr of next
-        ld a, iyh
-        ld (ix+history_entry_next+1), a                 ; addr of next
-        ld (ix+history_entry_line_len), c
-        ; write into previous entry the address of new entry
-        ld iy, hl                                       ; load hl into iy for index adressing
-        ld a, ixl
-        ld (iy+history_entry_next), a
-        ld a, ixh
-        ld (iy+history_entry_next+1), a
-
+        ld a, (hl)
+        ld (de), a
+        ; former_entry.next[0] = E
+        ld (hl), e
+        ; A = former_entry.next[1]
+        ; former_entry.next[1] = D
+        inc hl
+        ld a, (hl)
+        ld (hl), d
+        ; new_entry.next[1] = A
+        inc de
+        ld (de), a
         ; copy line (with wrap-around handling and null terminator check)
-        ld hl, ix                               ; HL = destination address (IX)
-        add hl, history_entry_line_ptr          ; HL = destination address + offset for string
+        ; put the new entry in DE, and the string in HL
+        inc de
+        ; DE points to the size
+        pop bc
+        ld a, c
+        ld (de), a
+        inc de
+        ; DE points to the new entry char array, get the string from the stack
+        pop hl
 _add_history_add_entry_copy_loop:
-        ld a, (de)                              ; Load a byte from the string
-        cp 0                                    ; Check for null terminator
-        ld (hl), a                              ; Write the byte to the ring buffer
+        ld a, (hl)                              ; Load a byte from the string
+        ldi                                     ; Copy from HL to DE
+        or a
         jr z, _add_history_success              ; Return from the function if null terminator is encountered
-        inc de                                  ; Increment DE (source address)
-        inc hl                                  ; Increment HL (destination address)
-        ON_HL_BEYOND_HISTORY_RINBUFFER(_add_history_set_hl_to_ringbuffer_start, _add_history_add_entry_copy_loop)
-_add_history_set_hl_to_ringbuffer_start:
+        ON_REG_VALID_GOTO(D, E, _add_history_add_entry_copy_loop)
         ld hl, history_ringbuffer               ; Wrap around to the beginning of the buffer
         jr _add_history_add_entry_copy_loop
 _add_history_first_entry:
-        ld ix, history_ringbuffer
-        ld hl, ix
-        ld (ix+history_entry_next), l                   ; copy addres to self
-        ld (ix+history_entry_next+1), h
-        ld (ix+history_entry_line_len), c               ; line length with NULL byte
-        ; copy line
-        ld hl, ix
-        add hl, history_entry_line_ptr                  ; hl - dest & de - string to copy
-        ex de, hl                                       ; de - dest & hl - string to copy
-        ld b, 0                                         ; bc - length of string with null-byte
-        ldir
         ld hl, history_ringbuffer
-        ld (history_current_ptr), hl                    ; point to the first entry
+        ld (history_current_ptr), hl                        ; point to the first entry
+        ld (history_ringbuffer + history_entry_next), hl    ; copy address to self
+        ld a, c
+        ld (history_ringbuffer + history_entry_line_len), a ; line length with NULL byte
+        ; copy line
+        ld hl, history_ringbuffer + history_entry_line_ptr  ; hl - dest & de - string to copy
+        ex de, hl                                           ; de - dest & hl - string to copy
+        ; B should already be 0 here
+        ldir
 _add_history_success:
         call zealline_reset_history_search
         ld a, ERR_SUCCESS
+_add_history_ret:
         pop bc
         pop de
         pop hl
         ret
 _add_history_error:
         ld a, ERR_FAILURE
-        pop bc
-        pop de
-        pop hl
-        ret
+        jr _add_history_ret
 
 
         ; ---------------------------------------------------------------------
         ; PRIVATE_FUNCTIONS (all to be call'ed)
         ; ---------------------------------------------------------------------
 
- ___just_for_tooltip:
-
         ; Turns the iterator one entry backwards
         ; Returns: BC - value of (history_iterator_pr)
         ; Alters: A, BC, DE, HL
 history_iterator_back:
         ld hl, (history_iterator_ptr)
-        ON_HL_IS_NULL_GOTO(_history_iterator_back_use_current_ptr)
         ld a, h
         or l
-        call z, zealline_reset_history_search           ; also loads HL
-        ld de, hl                                       ; store HL in DE, our destinatin if the prev element was found
+        jr z, _history_iterator_back_use_current_ptr
+        ld d, h                                         ; store HL in DE, our destination if the prev element was found
+        ld e, l
         ASSERT history_entry_next==0                    ; so we can short-cut and use (hl) to load the next node
-        ld bc, hl                                       ; just for the loop
 _history_iterator_back_loop:
-        ld hl, bc
-        ld bc, (hl)                                     ; load next node into BR
-        DE_NE_BC(_history_iterator_back_loop)           ; if next node is equal to DE then we have found it
-        ld (history_iterator_ptr), hl
+        ld b, h
+        ld c, l
+        ; current = current.next where current is HL
+        ld a, (hl)
+        inc l
+        ld h, (hl)
+        ld l, a
+        ; Compare HL and DE, A already contains L
+        cp e
+        jr nz, _history_iterator_back_loop
+        ld a, h
+        cp d
+        jr nz, _history_iterator_back_loop              ; if next node is equal to DE then we have found it
+        ; BC is the previous node address
+        ld (history_iterator_ptr), bc
         ret
 _history_iterator_back_use_current_ptr:
         ld hl, (history_current_ptr)
         ld (history_iterator_ptr), hl
         ret
+
 
         ; Turns the iterator one entry forward
         ; Alters: A, BC, HL
@@ -307,26 +321,33 @@ history_iterator_forward:
         jp nz, _history_iterator_forward_get_next
         ld hl, (history_current_ptr)
 _history_iterator_forward_get_next:
-        ld bc, (hl)
-        ld (history_iterator_ptr), bc
+        ld a, (hl)
+        inc l                                           ; HL is aligned on 4 for sure
+        ld h, (hl)
+        ld l, a
+        ld (history_iterator_ptr), hl
         ret
 
-        ; copy the current iterator entry to history_search_result
+
+        ; Copy the current iterator entry to history_search_result
         ; Alters: A, BC, DE, HL, IX
 copy_iterator_to_search_result:
-        ld ix, (history_iterator_ptr)
+        ld hl, (history_iterator_ptr)
+        ; HL is algined on 4 for sure
+    REPT history_entry_line_len
+        inc l
+    ENDR
+        ld c, (hl)
         ld b, 0
-        ld c, (ix+history_entry_line_len)
-        ld hl, ix
-        add hl, history_entry_line_ptr
+        ASSERT(history_entry_line_ptr == history_entry_line_len + 1)
+        inc l
         ld de, history_search_result
 _copy_iterator_to_search_result_copy_loop:
         ldi
         ld a, c
         or a
         ret z
-        ON_HL_BEYOND_HISTORY_RINBUFFER(_copy_iterator_to_search_result_fix_hl, _copy_iterator_to_search_result_copy_loop)
-_copy_iterator_to_search_result_fix_hl:
+        ON_REG_VALID_GOTO(H, L, _copy_iterator_to_search_result_copy_loop)
         ld hl, history_ringbuffer
         jr _copy_iterator_to_search_result_copy_loop
 
@@ -337,18 +358,23 @@ _copy_iterator_to_search_result_fix_hl:
         ;       B - required space
         ; Returns:
         ;       A - 0 if we have enough space
-        ;       A - 1 if we dont have enough space
+        ;       A - non-zero if we dont have enough space
+        ; Alters:
+        ;       DE
 is_history_space_available:
-        push de
         push hl
         ASSERT history_entry_next==0
-        ld ix, (history_current_ptr)            ; Address of current Node
-        ld de, (ix)                             ; Address of the Next Node (where next of current is actually the oldest)
-        or a            ; Remove carry flag
-        ex hl, de       ; SWAP HL <-> DE
-        sbc hl, de      ; Calculate the difference between DE (start address) and HL (oldest element)
+        ; Put the address we want to write to in DE
+        ex hl, de
+        ld hl, (history_current_ptr)            ; Address of current Node
+        ld a, (hl)
+        inc l                                   ; HL was aligned on 4 for sure
+        ld h, (hl)
+        ld l, a
+        or a                                    ; Remove carry flag
+        sbc hl, de                              ; Calculate the difference between DE (start address) and HL (oldest element)
         jp p, _history_space_available_positive ; If the result is negative, we need to wrap around
-        add hl, HISTORY_SIZE                    ; therefore we add HISTORY_SIZE, now HL is thenumber of free bytes
+        add hl, HISTORY_SIZE                    ; therefore we add HISTORY_SIZE, now HL is the number of free bytes
 _history_space_available_positive:
         ; Compare the available space (HL) with the required space (B)
         ld a, h
@@ -356,16 +382,11 @@ _history_space_available_positive:
         jp nz, _history_space_available
         ld a, l            ; Compare the low byte of HL with B
         cp b
-        jr c, _history_space_not_available  ; Not enough space
+        jr c, _history_space_not_available  ; Not enough space, A is not 0 for sure
 _history_space_available:
         xor a              ; Set A to 0
-        pop hl
-        pop de
-        ret
 _history_space_not_available:
-        ld a, 1            ; Set A to a non-zero value (e.g., 1)
         pop hl
-        pop de
         ret
 
 
